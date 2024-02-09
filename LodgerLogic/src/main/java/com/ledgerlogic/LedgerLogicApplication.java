13525fda8f78e2cc96bba8e1841aff2e4952ae6b
Abderrahim Bahia <52228409+abderrahimbahia@users.noreply.github.com>
2024-02-09 10:47:51 -0500

    Add files via upload

---
+++ LodgerLogic/src/main/java/com/ledgerlogic/LedgerLogicApplication.java
@@ -0,0 +1,13 @@
+package com.ledgerlogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedgerLogicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LedgerLogicApplication.class, args);
+	}
+
+}
