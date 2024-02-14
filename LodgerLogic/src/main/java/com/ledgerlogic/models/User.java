792f45ae34f1cc46df3b259cc22d3aa7bd893fe1
Harrison Rosser <Hrosser15@gmail.com>
2024-02-13 20:59:17 -0500

    Multiple important fixes. Please see for important info:
    Changed the security question id fields to just be security question answers. rather than having a separate entity for security questions. We will just store the security answers in the user table.
    
    Fixed issue where failedLoginAttempt couldn't be null.
    
    Added in NON NULL annotations for some fields that shouldn't ever be null.
    
    Revised constructor to set up user with all required fields.

--- LodgerLogic/src/main/java/com/ledgerlogic/models/User.java
+++ LodgerLogic/src/main/java/com/ledgerlogic/models/User.java
@@ -15,38 +15,93 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
     private Long   userId;
-    private String username;
-    private String firstName;
-    private String lastName;
-    private String email;
-    private String password;
-    private String role = "accountant";
-    private String streetAddress;
-    private String city;
-    private String state;
-    private String zipCode;
-    private Date   birthDay;
-    private Boolean status = true; //true = active & false = inactive
-    private Date   passwordExpirationDate;
-    private short  failedLoginAttempt;
-    private Date   suspensionStartDate;
-    private Date   suspensionEndDate;
-    private Date   lastLoginDate;
-    private Date   accountCreationDate;
-    private int    securityQ1Id;
-    private int    securityQ2Id;
-    private int    securityQ3Id;
 
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+
+    @Column(nullable = false)
+    private String streetAddress;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String state;
+
+    @Column(nullable = false)
+    private String zipCode;
+
+    @Column(nullable = false)
+    private Date   birthDay;
+
+    @Column(nullable = false)
+    private Boolean status = true; //true = active & false = inactive
+
+    @Column(nullable = false)
+    private Date   passwordExpirationDate;
+
+    @Column(nullable = true)
+    private Short  failedLoginAttempt;  //can't be null if primitive, so changed short -> Short
+
+    @Column(nullable = true)
+    private Date   suspensionStartDate;
+
+    @Column(nullable = true)
+    private Date   suspensionEndDate;
+
+    @Column(nullable = true)
+    private Date   lastLoginDate;
+
+    @Column(nullable = false)
+    private Date   userCreationDate;
+
+    @Column(nullable = false)
+    private String    securityQ1Answer;
+
+    @Column(nullable = false)
+    private String    securityQ2Answer;
+
+    @Column(nullable = true)
     private String imageUrl;
 
-    public User(String firstName, String lastName, String email, String role, String username, String password){
+    public User(String username, String firstName, String lastName, String email, String password,
+                String role, String streetAddress, String city, String state, String zipCode,
+                Date birthDay, Boolean status, Date passwordExpirationDate, Date userCreationDate,
+                String securityQ1Answer, String securityQ2Answer) {
+        this.username = username;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
-        this.role = role;
-        this.username = username;
         this.password = password;
+        this.role = role;
+        this.streetAddress = streetAddress;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+        this.birthDay = birthDay;
+        this.status = status;
+        this.passwordExpirationDate = passwordExpirationDate;
+        this.userCreationDate = userCreationDate;
+        this.securityQ1Answer = securityQ1Answer;
+        this.securityQ2Answer = securityQ2Answer;
     }
+
 
 }
