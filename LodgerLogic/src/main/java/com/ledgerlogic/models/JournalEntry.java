94dea22908841bc6c42d5f5855105edd87a5cccf
Abderrahim Bahia <abderrahimbahia19@gmail.com>
2024-04-02 15:11:05 +0000

    email conteroller

---
+++ LodgerLogic/src/main/java/com/ledgerlogic/models/JournalEntry.java
@@ -0,0 +1,30 @@
+package com.ledgerlogic.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class JournalEntry {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long journalEntryId;
+    private BigDecimal credit;
+    private BigDecimal debit;
+
+    @OneToOne
+    private Account account;
+
+    public JournalEntry(BigDecimal credit, BigDecimal debit, Account account){
+        this.credit  = credit;
+        this.debit   = debit;
+        this.account = account;
+    }
+}
